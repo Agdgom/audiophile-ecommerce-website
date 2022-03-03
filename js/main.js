6e6158f14ddd8a32c654a582042e3c789962341c
David Agdgomelashvili <dato.agdgomelashvili2517@gmail.com>
2022-03-03 18:42:54 +0400

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 const cartBtn = document.querySelector('.cart');
-cartBtn.addEventListener('click', () => {
+if (cartBtn) {
   const cartModal = document.querySelector('.modal-cart');
-  cartModal.classList.toggle('hidden');
-});
+  cartBtn.addEventListener('click', () => {
+    if (cartModal) {
+      cartModal.classList.toggle('hidden');
+    }
+  });
+}
+
+const checkOutBtn = document.querySelector('.summary__btn');
+
+if (checkOutBtn) {
+  const checkoutModal = document.querySelector('.check-modal');
+  const checkoutModalContent = document.querySelector('.check-modal__content');
+  checkOutBtn.addEventListener('click', () => {
+    if (checkoutModal) {
+      checkoutModal.classList.toggle('hidden');
+      checkoutModalContent.classList.toggle('hidden');
+    }
+  });
+  checkoutModal.addEventListener('click', ({ target }) => {
+    if (checkoutModalContent && target === checkoutModal) {
+      checkoutModal.classList.add('hidden');
+      checkoutModalContent.classList.add('hidden');
+    }
+  });
+}
+
+// Menu
+const burgerIcon = document.querySelector('.icon-menu');
+if (burgerIcon) {
+  burgerIcon.addEventListener('click', () => {
+    const menuBody = document.querySelector('.menu__body');
+    if (menuBody) {
+      burgerIcon.classList.toggle('close-icon');
+      menuBody.classList.toggle('active');
+    }
+  });
+}
